41e3c7c6cddfa69bccf2a71058a0e251b7856bd6
MoniqChege <quichege33@gmail.com>
2025-04-09 15:59:37 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { SigninComponent } from "./signin/signin.component";
 import { SignupComponent } from "./signup/signup.component";
 import { TaskCreationComponent } from "./task-creation/task-creation.component";
@@ -7,10 +9,12 @@ import { TaskEditDeleteComponent } from "./task-edit-delete/task-edit-delete.com
 
 @Component({
   selector: 'app-root',
-  imports: [SigninComponent, SignupComponent, TaskCreationComponent, TaskEditDeleteComponent],
+  imports: [RouterModule, SigninComponent, SignupComponent, TaskCreationComponent, TaskEditDeleteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-assignment';
+
+  
 }
